5a34e6819d8436ea39c33c518e0825c7d185d59b
Md Mezbah Uddin <extraordinarymisbah@gmail.com>
2024-12-08 21:25:47 +0600

    created about us section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import About from "@/components/home/About";
 import HeroSection from "@/components/home/Hero";
 
 export default function Home() {
   return (
-    <div className="min-h-screen text-xl">
+    <div className="min-h-screen text-lg">
       <HeroSection />
+      <About />
     </div>
   );
 }
---
+++ src/components/home/About.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="bg-base-200 py-10">
+      <div className="container-center">
+        <h2 className="mb-5 text-center text-3xl font-bold lg:text-4xl">
+          About Us
+        </h2>
+        <div className="mx-auto max-w-[900px] space-y-2">
+          <p className="text-justify">
+            Welcome to{" "}
+            <span className="font-semibold text-primary opacity-100">
+              Arban Public School
+            </span>
+            , your trusted place for quality education since 2001. For over 20
+            years, we have been helping students grow into confident,
+            knowledgeable, and responsible individuals.
+          </p>
+
+          <p className="text-justify">
+            At{" "}
+            <span className="font-semibold text-primary opacity-100">
+              {" "}
+              Arban Public School
+            </span>
+            , we focus on creating a friendly and supportive environment where
+            every student can learn and thrive. Our dedicated teachers use
+            modern teaching methods to make learning fun and meaningful. Along
+            with academics, we help students develop important life skills and
+            strong values.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
