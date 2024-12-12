6ba4c10df779abd9fbd2b2be982a003c548d1875
Md Mezbah Uddin <extraordinarymisbah@gmail.com>
2024-12-12 13:42:20 +0600

    created principals message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import About from "@/components/home/About";
 import HeroSection from "@/components/home/Hero";
+import PrincipalsMessage from "@/components/home/Principal";
 
 export default function Home() {
   return (
-    <div className="min-h-screen text-lg">
+    <div className="min-h-screen space-y-9 pb-9 text-lg lg:space-y-12">
       <HeroSection />
       <About />
+      <PrincipalsMessage />
     </div>
   );
 }
