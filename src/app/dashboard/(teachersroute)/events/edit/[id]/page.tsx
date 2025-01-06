b254647b93ae7000a557deaf426cee77a4deec0c
Md Mezbah Uddin <extraordinarymisbah@gmail.com>
2025-01-06 22:01:39 +0600

    working on event update functionality

--- src/app/dashboard/(teachersroute)/events/edit/[id]/page.tsx
+++ src/app/dashboard/(teachersroute)/events/edit/[id]/page.tsx
@@ -1,19 +1,99 @@
 "use client";
 import Spinner from "@/components/shared/Spinner";
+import { useGetEventByIdQuery } from "@/redux/features/events/eventsApiSlice";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { RxCross1 } from "react-icons/rx";
 
+type EventType = {
+  title: string;
+  description: string;
+  images: string[];
+};
 const EventEditPage = ({ params }: { params: Promise<{ id: string }> }) => {
   const [eventId, setEventId] = useState<null | string>(null);
+  const [event, setEvent] = useState<null | EventType[]>(null);
+  const { data: fetchedEvent, isLoading } = useGetEventByIdQuery(eventId, {
+    skip: !eventId,
+  });
+
+  const { register, handleSubmit, reset } = useForm<EventType>();
 
   useEffect(() => {
     params.then((res) => {
       setEventId(res.id);
+      if (fetchedEvent) {
+        setEvent(fetchedEvent);
+        reset(fetchedEvent);
+      }
     });
-  }, [params]);
+  }, [params, reset, fetchedEvent]);
 
-  if (!eventId) return <Spinner />;
+  if (!eventId || isLoading) return <Spinner />;
 
-  return <div>{eventId}</div>;
+  const onSubmit = async (data: EventType) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        <div>
+          <label className="block text-sm font-medium">Title</label>
+          <input
+            type="text"
+            {...register("title", { required: "Title is required" })}
+            placeholder=""
+            className="input input-bordered w-full"
+          />
+        </div>
+
+        <div>
+          <label className="block text-sm font-medium">Description</label>
+          <textarea
+            {...register("description", {
+              required: "Description is required",
+            })}
+            className="textarea textarea-bordered min-h-[300px] w-full"
+          />
+        </div>
+
+        <div>
+          <label className="block text-sm font-medium">Images</label>
+          <div className="grid max-w-[480px] gap-2 sm:grid-cols-2">
+            {fetchedEvent?.images?.map((img: string) => {
+              return (
+                <div key={img} className="relative">
+                  <Image
+                    unoptimized
+                    key={img}
+                    src={img}
+                    alt="Event Image"
+                    height={100}
+                    width={100}
+                    className="w-full"
+                  />
+                  <RxCross1 className="absolute right-0 top-0 bg-black p-1 text-xl text-white" />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          //disabled={isSubmitting || isUpdatingNotice}
+          className="btn btn-primary mt-10"
+        >
+          {/* {(isSubmitting || isUpdatingNotice) && (
+            <span className="loading loading-spinner"></span>
+          )} */}
+          {/* {isSubmitting ? "Submitting..." : "Update Notice"} */}
+          Submit button
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default EventEditPage;
