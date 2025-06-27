8437b45c7b2e50c50978f06876d224151b976286
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-06-27 22:15:36 +0600

    updated contact ui

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import ContactForm from "../shared/ContactForm";
-import SectionTitle from "../shared/SectionTitle";
 
 const Contact = () => {
   return (
     <div className="container-center">
-      <SectionTitle text="Contact Us" className="mb-5" />
       <ContactForm />
     </div>
   );
