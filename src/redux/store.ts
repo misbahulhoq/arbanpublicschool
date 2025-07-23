ba5941b42bc33731e996b26bcc6bc06d5c66d52e
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-07-23 19:24:19 +0600

    CTA section overlap issue fixed.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -5,6 +5,7 @@ const rootReducer = combineReducers({
   authToken: "",
   [baseApi.reducerPath]: baseApi.reducer,
 });
+
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => {
